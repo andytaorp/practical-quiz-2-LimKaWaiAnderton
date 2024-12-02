aec21b86d32bd81f50fa6535b28b4c44f97c8df4
Anderton <23054103@myrp.edu.sg>
2024-12-02 14:18:44 +0800

    Commit

--- src/components/AddHabitForm.js
+++ src/components/AddHabitForm.js
@@ -4,10 +4,19 @@ export default function AddHabitForm({ onAddHabit }) {
   const [habitName, setHabitName] = useState("");
 
   const handleSubmit = (e) => {
-    // TODO: write code to handle form submission
-  };
+        // TODO: write code to handle form submission
+        e.preventDefault();
+        if (habitName.trim()) {
+            onAddHabit(habitName);
+            setHabitName("")
+        }
+    };
 
-  return {
-    //TODO: add a form to add a new habit
-  };
+    return (
+        //TODO: add a form to add a new habit
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={habitName} onChange={(e) => setHabitName(e.target.value)} placeholder="New Habit" />
+            <button type="submit">Add Habit</button>
+        </form>
+    );
 }
